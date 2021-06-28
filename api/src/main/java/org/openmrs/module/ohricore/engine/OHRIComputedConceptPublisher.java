ac6284368f8dc1a67e44d85d74d6d07f8f94fbff
smallgod <davies.mugume@gmail.com>
2021-06-28 13:36:58 +0300

    WIT - for review

--- api/src/main/java/org/openmrs/module/ohricore/engine/OHRIComputedConceptPublisher.java
+++ api/src/main/java/org/openmrs/module/ohricore/engine/OHRIComputedConceptPublisher.java
@@ -1,20 +1,19 @@
 package org.openmrs.module.ohricore.engine;
 
-import org.openmrs.module.ohricore.engine.OHRIComputedConcept;
-import org.openmrs.module.ohricore.engine.String3ComputedConcept;
-
 /**
  * @author smallGod date: 24/06/2021
  */
 public interface OHRIComputedConceptPublisher {
-	
-	/**
-	 * A list of ComputedConcept processes to publish
-	 */
-	OHRIComputedConcept[] SERVICES = { new String3ComputedConcept() };
-	
-	/**
-	 * Number of published services available
-	 */
-	int NUM_OF_SERVICES = SERVICES.length;
+
+    /**
+     * A list of ComputedConcept processes to publish
+     */
+    OHRIComputedConceptOLD[] SERVICES = {
+            new String3ComputedConcept()
+    };
+
+    /**
+     * Number of published services available
+     */
+    int NUM_OF_SERVICES = SERVICES.length;
 }
