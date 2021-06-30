fbad64f9e0244325c2ca968282538837a3e94cb9
smallgod <davies.mugume@gmail.com>
2021-06-30 23:28:42 +0300

    OHRI-144 Class constants to hold concepts

--- api/src/main/java/org/openmrs/module/ohricore/api/impl/String3ComputedConcept.java
+++ api/src/main/java/org/openmrs/module/ohricore/api/impl/String3ComputedConcept.java
@@ -9,48 +9,47 @@ import org.openmrs.module.ohricore.api.OHRIComputedConcept;
 import org.springframework.stereotype.Component;
 
 /**
- * @author MayanjaXL, Amos, Stephen, smallGod date: 28/06/2021
+ * @author MayanjaXL, Amos, Stephen, smallGod
+ * date: 28/06/2021
  */
 
 @Component("string3ComputedConcept")
 public class String3ComputedConcept implements OHRIComputedConcept {
-	
-	private Log log = LogFactory.getLog(this.getClass());
-	
-	@Override
-	public Obs compute(Encounter triggeringEncounter) {
-		
-		String string1 = null;
-		String string2 = null;
-		for (Obs observation : triggeringEncounter.getObs()) {
-			
-			String value = observation.getValueText();
-			if (value == null || value.trim().isEmpty()) {
-				value = String.valueOf(observation.getValueNumeric());
-			}
-			
-			String conceptUIID = observation.getConcept().getUuid();
-			if (conceptUIID.equals(String3ConceptUUID.STRING1)) {
-				string1 = value;
-			} else if (conceptUIID.equals(String3ConceptUUID.STRING2)) {
-				string2 = value;
-			}
-		}
-		
-		String string3Val = null;
-		if (!(string1 == null || string2 == null)) {
-			string3Val = string1 + " - " + string2;
-		}
-		return createOrUpdate(triggeringEncounter, string3Val);//voids str3 if either str1 or str2 is Null
-	}
-	
-	@Override
-	public Concept getConcept() {
-		return Context.getConceptService().getConceptByUuid(String3ConceptUUID.STRING3);
-	}
-	
-	@Override
-	public EncounterType getTargetEncounterType() {
-		return null;
-	}
+
+    @Override
+    public Obs compute(Encounter triggeringEncounter) {
+
+        String string1 = null;
+        String string2 = null;
+        for (Obs observation : triggeringEncounter.getObs()) {
+
+            String value = observation.getValueText();
+            if (value == null || value.trim().isEmpty()) {
+                value = String.valueOf(observation.getValueNumeric());
+            }
+
+            String conceptUIID = observation.getConcept().getUuid();
+            if (conceptUIID.equals(String3ConceptUUID.STRING1)) {
+                string1 = value;
+            } else if (conceptUIID.equals(String3ConceptUUID.STRING2)) {
+                string2 = value;
+            }
+        }
+
+        String string3Val = null;
+        if (!(string1 == null || string2 == null)) {
+            string3Val = string1 + " - " + string2;
+        }
+        return createOrUpdate(triggeringEncounter, string3Val);//voids str3 if either str1 or str2 is Null
+    }
+
+    @Override
+    public Concept getConcept() {
+        return Context.getConceptService().getConceptByUuid(String3ConceptUUID.STRING3);
+    }
+
+    @Override
+    public EncounterType getTargetEncounterType() {
+        return null;
+    }
 }
