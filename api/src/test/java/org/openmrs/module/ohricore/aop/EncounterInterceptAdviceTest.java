a184fcb0e330b52fa40eac765f9947a95f128152
smallgod <davies.mugume@gmail.com>
2021-07-12 13:15:34 +0300

    OHRI-146 delete again all tracked files and re-track

--- api/src/test/java/org/openmrs/module/ohricore/aop/EncounterInterceptAdviceTest.java
+++ api/src/test/java/org/openmrs/module/ohricore/aop/EncounterInterceptAdviceTest.java
@@ -14,40 +14,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
- * @author MayanjaXL, Amos, Stephen, smallGod
- * date: 18/06/2021
+ * @author MayanjaXL, Amos, Stephen, smallGod date: 18/06/2021
  */
 
 public class EncounterInterceptAdviceTest extends BaseModuleContextSensitiveTest {
-
-    protected static final String ENCOUNTER_DATA_XML = "org/openmrs/module/ohricore/include/BasicEncounter-initialData.xml";
-
-    protected static final String CONCEPTS_DATA_XML = "org/openmrs/module/ohricore/include/HIVComputedConcept-initConceptData.xml";
-
-    protected static final String TEST_ENCOUNTER_UUID = "430bbb70-6a9c-4e1e-badb-9d1034b1b5e9";
-
-    private Method methodInvoked;
-
-    @Autowired
-    private EncounterService encounterService;
-
-    @Before
-    public void setUpEncounter() throws ClassNotFoundException, NoSuchMethodException {
-
-        executeDataSet(ENCOUNTER_DATA_XML);
-        executeDataSet(CONCEPTS_DATA_XML);
-
-        Class clazz = Class.forName("org.openmrs.api.impl.EncounterServiceImpl");
-        methodInvoked = clazz.getDeclaredMethod(ConceptComputeTrigger.SAVE_ENCOUNTER, Encounter.class);
-    }
-
-    @Test
-    public void afterReturning_shouldCompute() throws Throwable {
-
-        Encounter encounter = encounterService.getEncounterByUuid(TEST_ENCOUNTER_UUID);
-        assertNotNull(encounter, "Test Encounter should be available");
-        assertNotNull(methodInvoked);
-        assertEquals("saveEncounter", methodInvoked.getName());
-        new EncounterInterceptorAdvice().afterReturning(null, methodInvoked, new Object[]{encounter}, null);
-    }
+	
+	protected static final String ENCOUNTER_DATA_XML = "org/openmrs/module/ohricore/include/BasicEncounter-initialData.xml";
+	
+	protected static final String CONCEPTS_DATA_XML = "org/openmrs/module/ohricore/include/HIVComputedConcept-initConceptData.xml";
+	
+	protected static final String TEST_ENCOUNTER_UUID = "430bbb70-6a9c-4e1e-badb-9d1034b1b5e9";
+	
+	private Method methodInvoked;
+	
+	@Autowired
+	private EncounterService encounterService;
+	
+	@Before
+	public void setUpEncounter() throws ClassNotFoundException, NoSuchMethodException {
+		
+		executeDataSet(ENCOUNTER_DATA_XML);
+		executeDataSet(CONCEPTS_DATA_XML);
+		
+		Class clazz = Class.forName("org.openmrs.api.impl.EncounterServiceImpl");
+		methodInvoked = clazz.getDeclaredMethod(ConceptComputeTrigger.SAVE_ENCOUNTER, Encounter.class);
+	}
+	
+	@Test
+	public void afterReturning_shouldCompute() throws Throwable {
+		
+		Encounter encounter = encounterService.getEncounterByUuid(TEST_ENCOUNTER_UUID);
+		assertNotNull(encounter, "Test Encounter should be available");
+		assertNotNull(methodInvoked);
+		assertEquals("saveEncounter", methodInvoked.getName());
+		new EncounterInterceptorAdvice().afterReturning(null, methodInvoked, new Object[] { encounter }, null);
+	}
 }
