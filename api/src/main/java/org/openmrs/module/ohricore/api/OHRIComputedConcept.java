db4fe5bbe1be57279e1c82196188fb715ba90a22
smallgod <davies.mugume@gmail.com>
2021-07-13 02:24:55 +0300

    OHRI-146 Added more test data files

--- api/src/main/java/org/openmrs/module/ohricore/api/OHRIComputedConcept.java
+++ api/src/main/java/org/openmrs/module/ohricore/api/OHRIComputedConcept.java
@@ -11,6 +11,8 @@ import org.openmrs.module.ohricore.engine.CommonsUUID;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Random;
 
 /**
  * @author MayanjaXL, Amos, Stephen, smallGod
