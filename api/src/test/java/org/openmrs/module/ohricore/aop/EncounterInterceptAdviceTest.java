0a4e8544da3d99735e02e48083778e1ba6029f8f
smallgod <davies.mugume@gmail.com>
2021-07-14 19:33:04 +0300

    OHRI-146 Delete unused test class

--- api/src/test/java/org/openmrs/module/ohricore/aop/EncounterInterceptAdviceTest.java
+++
@@ -1,9 +0,0 @@
-package org.openmrs.module.ohricore.aop;
-
-import org.openmrs.test.BaseModuleContextSensitiveTest;
-
-/**
- * @author MayanjaXL, Amos, Stephen, smallGod date: 18/06/2021
- */
-
-public class EncounterInterceptAdviceTest extends BaseModuleContextSensitiveTest {}
