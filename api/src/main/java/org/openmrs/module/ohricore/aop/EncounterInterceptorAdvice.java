cf54b08e9d62e56bd5cba9e1d116303c02022716
smallgod <davies.mugume@gmail.com>
2021-07-14 19:33:04 +0300

    OHRI-146 Delete unused test class

--- api/src/main/java/org/openmrs/module/ohricore/aop/EncounterInterceptorAdvice.java
+++ api/src/main/java/org/openmrs/module/ohricore/aop/EncounterInterceptorAdvice.java
@@ -13,22 +13,22 @@ import java.util.List;
  * @author MayanjaXL, Amos, Stephen, smallGod date: 16/06/2021
  */
 public class EncounterInterceptorAdvice implements AfterReturningAdvice {
-
-    @Override
-    public void afterReturning(Object returnValue, Method methodInvoked, Object[] methodArgs, Object target)
-            throws Throwable {
-
-        if (methodInvoked.getName().equals(ConceptComputeTrigger.SAVE_ENCOUNTER)) {
-            for (Object arg : methodArgs) {
-                if (arg instanceof Encounter) {
-                    Encounter encounter = (Encounter) arg;
-                    List<OHRIComputedConcept> ohriComputedConcepts = OHRIComputedConceptsFactory
-                            .getComputedConcepts(encounter);
-                    for (OHRIComputedConcept computedConcept : ohriComputedConcepts) {
-                        computedConcept.computeAndPersistObs(encounter);
-                    }
-                }
-            }
-        }
-    }
+	
+	@Override
+	public void afterReturning(Object returnValue, Method methodInvoked, Object[] methodArgs, Object target)
+	        throws Throwable {
+		
+		if (methodInvoked.getName().equals(ConceptComputeTrigger.SAVE_ENCOUNTER)) {
+			for (Object arg : methodArgs) {
+				if (arg instanceof Encounter) {
+					Encounter encounter = (Encounter) arg;
+					List<OHRIComputedConcept> ohriComputedConcepts = OHRIComputedConceptsFactory
+					        .getComputedConcepts(encounter);
+					for (OHRIComputedConcept computedConcept : ohriComputedConcepts) {
+						computedConcept.computeAndPersistObs(encounter);
+					}
+				}
+			}
+		}
+	}
 }
--- api/src/main/java/org/openmrs/module/ohricore/engine/ConceptComputeTrigger.java
+++ api/src/main/java/org/openmrs/module/ohricore/engine/ConceptComputeTrigger.java
@@ -4,5 +4,6 @@ package org.openmrs.module.ohricore.engine;
  * @author MayanjaXL, Amos, Stephen, smallGod date: 30/06/2021
  */
 public final class ConceptComputeTrigger {
-    public static final String SAVE_ENCOUNTER = "saveEncounter";
+	
+	public static final String SAVE_ENCOUNTER = "saveEncounter";
 }
