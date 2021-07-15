047b98414a6363e09d87d9c4b7d57b2e2da8d2ce
smallgod <davies.mugume@gmail.com>
2021-07-15 17:49:18 +0300

    OHRI-146 Include only files for Review

---
+++ api/src/main/java/org/openmrs/module/ohricore/api/impl/OnARTComputedConcept.java
@@ -0,0 +1,28 @@
+package org.openmrs.module.ohricore.api.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.ohricore.api.OHRIComputedConcept;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author MayanjaXL, Amos, Stephen, smallGod date: 28/06/2021
+ */
+@Component
+public class OnARTComputedConcept implements OHRIComputedConcept {
+	
+	@Override
+	public org.openmrs.Obs compute(org.openmrs.Encounter triggeringEncounter) {
+		return null;
+	}
+	
+	@Override
+	public org.openmrs.Concept getConcept() {
+		return null;
+	}
+	
+	@Override
+	public org.openmrs.EncounterType getTargetEncounterType() {
+		return null;
+	}
+}
