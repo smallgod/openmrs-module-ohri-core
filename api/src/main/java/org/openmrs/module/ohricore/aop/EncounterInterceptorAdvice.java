24ef41b7560eafb5ab119bdbcd24f394cddf77e4
smallgod <davies.mugume@gmail.com>
2021-08-26 17:59:12 +0300

    Correct HIV test date uuid

--- api/src/main/java/org/openmrs/module/ohricore/aop/EncounterInterceptorAdvice.java
+++ api/src/main/java/org/openmrs/module/ohricore/aop/EncounterInterceptorAdvice.java
@@ -1,7 +1,6 @@
 package org.openmrs.module.ohricore.aop;
 
 import org.openmrs.Encounter;
-import org.openmrs.aop.AuthorizationAdvice;
 import org.openmrs.module.ohricore.api.OHRIComputedConcept;
 import org.openmrs.module.ohricore.engine.ConceptComputeTrigger;
 import org.openmrs.module.ohricore.engine.OHRIComputedConceptsFactory;
@@ -13,32 +12,32 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 /**
- * @author MayanjaXL, Amos, Stephen, smallGod date: 16/06/2021
+ * @author MayanjaXL, Amos, Stephen, smallGod
+ * date: 16/06/2021
  */
 public class EncounterInterceptorAdvice implements AfterReturningAdvice {
-	
-	private static final Logger log = LoggerFactory.getLogger(EncounterInterceptorAdvice.class);
-	
-	@Override
-	public void afterReturning(Object returnValue, Method methodInvoked, Object[] methodArgs, Object target)
-	        throws Throwable {
-		
-		try {
-			if (methodInvoked.getName().equals(ConceptComputeTrigger.SAVE_ENCOUNTER)) {
-				for (Object arg : methodArgs) {
-					if (arg instanceof Encounter) {
-						Encounter encounter = (Encounter) arg;
-						List<OHRIComputedConcept> ohriComputedConcepts = OHRIComputedConceptsFactory
-						        .getComputedConcepts(encounter);
-						for (OHRIComputedConcept computedConcept : ohriComputedConcepts) {
-							computedConcept.computeAndPersistObs(encounter);
-						}
-					}
-				}
-			}
-		}
-		catch (Exception e) {
-			log.error("An un-expected Error occurred computing for computed concept");
-		}
-	}
+
+    private static final Logger log = LoggerFactory.getLogger(EncounterInterceptorAdvice.class);
+
+    @Override
+    public void afterReturning(Object returnValue, Method methodInvoked, Object[] methodArgs, Object target)
+            throws Throwable {
+
+        try {
+            if (methodInvoked.getName().equals(ConceptComputeTrigger.SAVE_ENCOUNTER)) {
+                for (Object arg : methodArgs) {
+                    if (arg instanceof Encounter) {
+                        Encounter encounter = (Encounter) arg;
+                        List<OHRIComputedConcept> ohriComputedConcepts = OHRIComputedConceptsFactory
+                                .getComputedConcepts(encounter);
+                        for (OHRIComputedConcept computedConcept : ohriComputedConcepts) {
+                            computedConcept.computeAndPersistObs(encounter);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("An un-expected Error occurred while computing for a computed concept");
+        }
+    }
 }
