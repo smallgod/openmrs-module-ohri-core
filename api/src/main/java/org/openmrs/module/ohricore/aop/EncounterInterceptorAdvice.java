01671578db7c691744d40a08ba9c35be28132c2a
smallGod <davies.mugume@gmail.com>
2022-04-07 16:00:14 +0300

    OHRI-800 Add encounter interceptor to update dead patient

--- api/src/main/java/org/openmrs/module/ohricore/aop/EncounterInterceptorAdvice.java
+++ api/src/main/java/org/openmrs/module/ohricore/aop/EncounterInterceptorAdvice.java
@@ -1,7 +1,12 @@
 package org.openmrs.module.ohricore.aop;
 
+import org.openmrs.Concept;
 import org.openmrs.Encounter;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.ohricore.api.OHRIComputedConcept;
+import org.openmrs.module.ohricore.engine.CommonsUUID;
 import org.openmrs.module.ohricore.engine.ConceptComputeTrigger;
 import org.openmrs.module.ohricore.engine.OHRIComputedConceptsFactory;
 import org.slf4j.Logger;
@@ -9,7 +14,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.aop.AfterReturningAdvice;
 
 import java.lang.reflect.Method;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static org.openmrs.module.ohricore.engine.CommonsUUID.CAUSE_OF_DEATH;
+import static org.openmrs.module.ohricore.engine.CommonsUUID.DATE_OF_DEATH;
+import static org.openmrs.module.ohricore.engine.CommonsUUID.DEATH_FORM_ENCOUNTER_TYPE;
+import static org.openmrs.module.ohricore.engine.ComputedConceptUtil.dateWithinPeriodFromNow;
 
 /**
  * @author MayanjaXL, Amos, Stephen, smallGod date: 16/06/2021
@@ -19,25 +35,51 @@ public class EncounterInterceptorAdvice implements AfterReturningAdvice {
 	private static final Logger log = LoggerFactory.getLogger(EncounterInterceptorAdvice.class);
 	
 	@Override
-	public void afterReturning(Object returnValue, Method methodInvoked, Object[] methodArgs, Object target)
-	        throws Throwable {
-		
-		try {
-			if (methodInvoked.getName().equals(ConceptComputeTrigger.SAVE_ENCOUNTER)) {
-				for (Object arg : methodArgs) {
-					if (arg instanceof Encounter) {
-						Encounter encounter = (Encounter) arg;
-						List<OHRIComputedConcept> ohriComputedConcepts = OHRIComputedConceptsFactory
-						        .getComputedConcepts(encounter);
-						for (OHRIComputedConcept computedConcept : ohriComputedConcepts) {
-							computedConcept.computeAndPersistObs(encounter);
-						}
-					}
-				}
-			}
-		}
-		catch (Exception e) {
-			log.error("An un-expected Error occurred while computing for a computed concept");
-		}
-	}
+    public void afterReturning(Object returnValue, Method methodInvoked, Object[] methodArgs, Object target)
+            throws Throwable {
+
+        try {
+            if (methodInvoked.getName().equals(ConceptComputeTrigger.SAVE_ENCOUNTER)) {
+                for (Object arg : methodArgs) {
+                    if (arg instanceof Encounter) {
+                        Encounter encounter = (Encounter) arg;
+                        List<OHRIComputedConcept> ohriComputedConcepts = OHRIComputedConceptsFactory
+                                .getComputedConcepts(encounter);
+                        for (OHRIComputedConcept computedConcept : ohriComputedConcepts) {
+                            computedConcept.computeAndPersistObs(encounter);
+                        }
+
+                        //TODO: Re-factor this code into a separate neat file
+                        if (Objects.equals(encounter.getEncounterType().getUuid(), DEATH_FORM_ENCOUNTER_TYPE)) {
+
+                            Supplier<Stream<Obs>> deathObs = encounter.getObs()::stream;
+                            Date date = deathObs.get()
+                                    .filter(obs -> obs.getConcept().getUuid().equals(DATE_OF_DEATH))
+                                    .findAny()
+                                    .map(Obs::getValueDate)
+                                    .orElse(null);
+
+                            if (date != null) {
+
+                                Supplier<Stream<Obs>> causeObs = encounter.getObs()::stream;
+                                Concept cause = causeObs.get()
+                                        .filter(obs -> obs.getConcept().getUuid().equals(CAUSE_OF_DEATH))
+                                        .findAny()
+                                        .map(Obs::getValueCoded)
+                                        .orElse(null);
+
+                                Patient client = encounter.getPatient();
+                                client.setCauseOfDeath(cause);
+                                client.setDead(Boolean.TRUE);
+                                client.setDeathDate(date);
+                                Context.getPatientService().savePatient(client);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("An un-expected Error occurred while computing for a computed concept");
+        }
+    }
 }
